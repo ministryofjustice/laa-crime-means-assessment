d9861d23ef2e622e8308b67e0974109bd00d53a3
Rajeshkumar Kothandaraman <rajeshkumar.kothandaraman@digital.justice.gov.uk>
2021-11-25 09:48:23 +0000

    Changes to add postgres support

---
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/config/CrimeMeansAssessmentConfiguration.java
@@ -0,0 +1,8 @@
+package uk.gov.justice.laa.crime.meansassessment.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class CrimeMeansAssessmentConfiguration {
+
+}
