df0802ac9cc9066d4162dd23dc2a86ceb663f7e6
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-11-26 09:09:36 +0000

    LASB-759: I've added remaining RESTful API endpoints for CRUD operations

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/defendant/controller/DefendantAssessmentController.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/defendant/controller/DefendantAssessmentController.java
@@ -9,10 +9,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.justice.laa.crime.meansassessment.defendant.entity.DefendantAssessmentEntity;
 import uk.gov.justice.laa.crime.meansassessment.defendant.service.DefendantAssessmentService;
 import uk.gov.justice.laa.crime.meansassessment.dto.ErrorDTO;
@@ -25,15 +23,53 @@ import uk.gov.justice.laa.crime.meansassessment.dto.ErrorDTO;
 public class DefendantAssessmentController {
 
     private final DefendantAssessmentService defendantAssessmentService;
-    @GetMapping("/get")
+
+    @GetMapping(value="/{defendantAssessmentId}")
     @Operation(description = "Defendant Means Assessment API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content),
             @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
             @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)))
     })
-    public ResponseEntity<DefendantAssessmentEntity> getDefendantAssessment(@RequestParam("defendantAssessmentId") String defendantAssessmentId) {
+    public ResponseEntity<DefendantAssessmentEntity> getDefendantAssessment(@PathVariable("defendantAssessmentId") String defendantAssessmentId) {
+        log.info("Retrieving DefendantAssessmentEntity with id {}",defendantAssessmentId);
         DefendantAssessmentEntity defendantAssessmentDTO = defendantAssessmentService.findById(defendantAssessmentId);
         return ResponseEntity.ok(defendantAssessmentDTO);
+    }
+
+    @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)))
+    })
+    public ResponseEntity<DefendantAssessmentEntity> createDefendantAssessmentEntity(@RequestBody DefendantAssessmentEntity defendantAssessmentEntity){
+        if (StringUtils.hasLength(defendantAssessmentEntity.getId())) {
+            return ResponseEntity.badRequest().body(defendantAssessmentEntity);
+        }
+        return ResponseEntity.ok(defendantAssessmentService.save(defendantAssessmentEntity));
+    }
+
+    @PutMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)))
+    })
+    public ResponseEntity<DefendantAssessmentEntity> updateDefendantAssessmentEntity(@RequestBody DefendantAssessmentEntity defendantAssessmentEntity){
+       return ResponseEntity.ok(defendantAssessmentService.update(defendantAssessmentEntity));
+    }
+
+    @DeleteMapping(value="/{defendantAssessmentId}")
+    @Operation(description = "Defendant Means Assessment API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)))
+    })
+    public ResponseEntity<String> deleteDefendantAssessment(@PathVariable("defendantAssessmentId") String defendantAssessmentId) {
+        log.info("deleting DefendantAssessmentEntity with id {}",defendantAssessmentId);
+
+        return ResponseEntity.ok(defendantAssessmentService.deleteById(defendantAssessmentId));
     }
 }
