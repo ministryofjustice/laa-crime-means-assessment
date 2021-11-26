40b8e5ffd0168e5e6e48cff7e0f057a55ae0cde1
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-11-26 09:09:36 +0000

    LASB-759: I've added remaining RESTful API endpoints for CRUD operations

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/defendant/service/DefendantAssessmentService.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/defendant/service/DefendantAssessmentService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.meansassessment.defendant.entity.DefendantAssessmentEntity;
 import uk.gov.justice.laa.crime.meansassessment.defendant.repository.DefendantAssessmentRepository;
 
+import java.util.UUID;
+
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,23 @@ public class DefendantAssessmentService {
 
     public DefendantAssessmentEntity findById(String defendantAssessmentId) {
         return defendantAssessmentRepository.findById(defendantAssessmentId).orElse(null);
+    }
+
+    public DefendantAssessmentEntity save(DefendantAssessmentEntity defendantAssessmentEntity){
+        defendantAssessmentEntity.setId(UUID.randomUUID().toString());
+        return defendantAssessmentRepository.save(defendantAssessmentEntity);
+    }
+
+    public DefendantAssessmentEntity update(DefendantAssessmentEntity defendantAssessmentEntity){
+        return defendantAssessmentRepository.save(defendantAssessmentEntity);
+    }
+
+    public String deleteById(String licensedefendantAssessmentIdId){
+        String responseMessage = null;
+        var defendantAssessmentEntity = new DefendantAssessmentEntity();
+        defendantAssessmentEntity.setId(licensedefendantAssessmentIdId);
+        defendantAssessmentRepository.delete(defendantAssessmentEntity);
+
+        return new StringBuilder("Successfully deleted license defendant with id: ").append(licensedefendantAssessmentIdId).toString();
     }
 }
---
+++ crime-means-assessment/src/main/resources/db/changelog/changeset/00-mla-create-schema.sql
@@ -0,0 +1,3 @@
+--liquibase formatted sql
+--changeset lucast:00-mla-create-schema.sql
+CREATE SCHEMA IF NOT EXISTS mla AUTHORIZATION postgres;
