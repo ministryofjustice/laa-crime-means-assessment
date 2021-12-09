5dd37f1b1c4d2c69b3b632782bcdaeb33173625a
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-12-09 14:53:17 +0000

    LASB-742: I've deleted a custom exception that is not necessary to have

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/defendant/exceptions/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.laa.crime.meansassessment.defendant.exceptions;
-
-public class ObjectNotFoundException  extends RuntimeException {
-    public ObjectNotFoundException(String objectId, String className){
-        super(new StringBuilder(className).append(" not found. ID: ").append(objectId).toString());
-    }
-}
