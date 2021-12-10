181b5fe29a7a02e65f981de218525cdb7411eaed
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-12-10 13:37:28 +0000

    LASB-766: I've added enum AssessmentStatus corresponding to DB tables HARDSHIP_REVIEW_STATUSES, IOJ_APPEAL_STATUSES, FIN_ASS_STATUSES

---
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/converter/AssessmentStatusConverter.java
@@ -0,0 +1,21 @@
+package uk.gov.justice.laa.crime.meansassessment.staticdata.enums.converter;
+
+import org.apache.commons.lang3.StringUtils;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.AssessmentStatus;
+
+import javax.persistence.AttributeConverter;
+
+public class HardshipReviewStatusConverter  implements AttributeConverter<AssessmentStatus, String> {
+
+    @Override
+    public String convertToDatabaseColumn(AssessmentStatus attribute) {
+        if (attribute == null) {return null; }
+        return attribute.getStatus();
+    }
+
+    @Override
+    public AssessmentStatus convertToEntityAttribute(String statusFromDB) {
+        if (StringUtils.isBlank(statusFromDB)) { return null; }
+        return AssessmentStatus.getFrom(statusFromDB);
+    }
+}
