eff240f8e8baf030ae3552e71bbb6628c5cb0723
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-12-20 11:47:58 +0000

    LASB-798: initial commit of initial means assessment api with json schema object creation and validation. not final commit.

---
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/model/common/ApiNewWorkReason.java
@@ -0,0 +1,45 @@
+
+package uk.gov.justice.laa.crime.meansassessment.model.common;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.processing.Generated;
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * The newWorkReason schema
+ * <p>
+ * An explanation about the purpose of this instance.
+ * 
+ */
+@Generated("jsonschema2pojo")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiNewWorkReason {
+
+    /**
+     * New Work reason code
+     * (Required)
+     * 
+     */
+    @SerializedName("code")
+    @Expose
+    @NotNull
+    public String code;
+    /**
+     * New work reason description
+     * 
+     */
+    @SerializedName("description")
+    @Expose
+    public String description;
+
+}
