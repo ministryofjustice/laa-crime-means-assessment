22df5fd47d9add94065c78a582b9562c40492123
Lucas T <lucas.toledo@digital.justice.gov.uk>
2021-12-23 16:31:30 +0000

    LASB-798: I removed stub data as it is not necessary.

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MeansAssessmentService.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MeansAssessmentService.java
@@ -34,51 +34,7 @@ public class MeansAssessmentService {
 
     public ApiCreateMeansAssessmentResponse createInitialAssessment(ApiCreateMeansAssessmentRequest meansAssessment){
         log.info("Create initial means assessment - Start");
-        var response = TemporaryCreateMeansAssessmentReponse.getCreateMeansAssessmentResponse();
 
-        return response;
-    }
-
-    private static class TemporaryCreateMeansAssessmentReponse{
-        private static ApiCreateMeansAssessmentResponse getCreateMeansAssessmentResponse() {
-            var mar = new ApiCreateMeansAssessmentResponse();
-            mar.setAssessmentId("7c49ebfe-fe3a-4f2f-8dad-f7b8f03b8327");
-            mar.setCriteriaId(41);
-            mar.setTotalAggregatedIncome(Double.valueOf("10.00"));
-            mar.setAdjustedIncomeValue(Double.valueOf("11.00"));
-            mar.setLowerThreshold(Double.valueOf("12.00"));
-            mar.setUpperThreshold(Double.valueOf("13.00"));
-            mar.setResult("testResult");
-            mar.setResultReason("testResultReason");
-            mar.setAssessmentStatus(getApiAssessmentStatus());
-            mar.setAssessmentSummary(getApiAssessmentSummaries());
-            mar.setTransactionDateTime(LocalDateTime.of(2022, 1, 5, 0, 0));
-
-            return mar;
-        }
-        private static ApiAssessmentStatus getApiAssessmentStatus(){
-            var assessmentStatus = new ApiAssessmentStatus();
-            assessmentStatus.setStatus("testStatus");
-            return assessmentStatus;
-        }
-        private static List<ApiAssessmentSummary> getApiAssessmentSummaries(){
-            var assessmentSummary = new ApiAssessmentSummary();
-            assessmentSummary.setApplicantAnnualTotal(Double.valueOf("14.00"));
-            assessmentSummary.setAnnualTotal(Double.valueOf("15.00"));
-            assessmentSummary.setAssessmentDetail(getAssessmentDetails());
-            return List.of(assessmentSummary);
-        }
-        private static List<ApiAssessmentDetail> getAssessmentDetails(){
-            var assessmentDetail = new ApiAssessmentDetail();
-            assessmentDetail.setCriteriaDetailsId("7c49ebfe-fe3a-4f2f-8dad-f7b8f03b8327");
-            assessmentDetail.setApplicantAmount(Double.valueOf("16.00"));
-            assessmentDetail.setApplicantFrequency(getFrequency());
-            return List.of(assessmentDetail);
-        }
-        private static ApiFrequency getFrequency(){
-            var frequency = new ApiFrequency();
-            frequency.setCode("test-code");
-            return frequency;
-        }
+        return null;
     }
 }
