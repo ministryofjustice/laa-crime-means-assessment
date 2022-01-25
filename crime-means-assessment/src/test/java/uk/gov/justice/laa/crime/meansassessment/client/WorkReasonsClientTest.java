528f8ef7c10a1aa2244c9201f087a887c5f15b05
atharmajeed <athar.majeed@digital.justice.gov.uk>
2022-01-25 21:59:22 +0000

    lasb-779 - implementing create init assessment validation and web client class

---
+++ crime-means-assessment/src/test/java/uk/gov/justice/laa/crime/meansassessment/client/WorkReasonsClientTest.java
@@ -0,0 +1,14 @@
+package uk.gov.justice.laa.crime.meansassessment.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkReasonsClientTest {
+
+    @Test
+    void checkWorkReasonStatus() {
+
+
+    }
+}
