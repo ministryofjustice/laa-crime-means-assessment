41f8e7191727c2cfc077f0e0fb94fc903207b306
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-02-22 13:26:00 +0000

    Use existing classes for request objects where possible.
    The enum classes Frequency, CaseType and CurrentStatus are now linked to the JSON schema files, which should simplify development and cut down on duplication.

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/CurrentStatus.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/CurrentStatus.java
@@ -1,21 +1,25 @@
 package uk.gov.justice.laa.crime.meansassessment.staticdata.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.validation.constraints.NotNull;
 import java.util.stream.Stream;
 /**
  * static data migrated from TOGDATA.HARDSHIP_REVIEW_STATUSES,
  * TOGDATA.IOJ_APPEAL_STATUSES and TOGDATA.FIN_ASS_STATUSES tables
  */
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public enum CurrentStatus {
     IN_PROGRESS("IN PROGRESS", "Incomplete"),
     COMPLETE("COMPLETE","Complete");
 
+    @NotNull
+    @JsonPropertyDescription("This will have the frequency code of the selection")
     private String status;
     private String description;
 
@@ -29,6 +33,6 @@ public enum CurrentStatus {
         return Stream.of(CurrentStatus.values())
                 .filter(f -> f.status.equals(status))
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Status with value: %s does not exist.", status)));
     }
 }
