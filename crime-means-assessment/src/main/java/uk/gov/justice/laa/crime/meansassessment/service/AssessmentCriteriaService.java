8caae15d18ae7d3ab8f32a1b39170722ae413466
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-02-23 10:55:24 +0000

    Implemented initial means assessment calculation

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/AssessmentCriteriaService.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/AssessmentCriteriaService.java
@@ -4,11 +4,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.meansassessment.exception.AssessmentCriteriaNotFoundException;
+import uk.gov.justice.laa.crime.meansassessment.exception.ValidationException;
+import uk.gov.justice.laa.crime.meansassessment.model.common.ApiAssessmentDetail;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaDetailEntity;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaDetailFrequencyEntity;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaEntity;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.CaseTypeAssessmentCriteriaDetailValueEntity;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.CaseType;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.Frequency;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.repository.AssessmentCriteriaDetailFrequencyRepository;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.repository.AssessmentCriteriaRepository;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.repository.CaseTypeAssessmentCriteriaDetailValueRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +26,8 @@ import java.time.LocalDateTime;
 public class AssessmentCriteriaService {
 
     private final AssessmentCriteriaRepository assessmentCriteriaRepository;
+    private final AssessmentCriteriaDetailFrequencyRepository assessmentCriteriaDetailFrequencyRepository;
+    private final CaseTypeAssessmentCriteriaDetailValueRepository caseTypeAssessmentCriteriaDetailValueRepository;
 
     protected AssessmentCriteriaEntity getAssessmentCriteria(LocalDateTime assessmentDate, boolean hasPartner, boolean contraryInterest) {
         AssessmentCriteriaEntity assessmentCriteriaForDate = assessmentCriteriaRepository.findAssessmentCriteriaForDate(assessmentDate);
@@ -28,6 +40,55 @@ public class AssessmentCriteriaService {
         } else {
             log.error("No Assessment Criteria found for date {}", assessmentDate);
             throw new AssessmentCriteriaNotFoundException(String.format("No Assessment Criteria found for date %s", assessmentDate));
+        }
+    }
+
+    protected void checkCriteriaDetailFrequency(AssessmentCriteriaDetailEntity criteriaDetail, Frequency frequency) {
+        Optional<AssessmentCriteriaDetailFrequencyEntity> detailFrequency =
+                assessmentCriteriaDetailFrequencyRepository.findByAssessmentCriteriaDetailAndFrequency(criteriaDetail, frequency);
+        if (detailFrequency.isEmpty()) {
+            throw new ValidationException(String.format("Frequency: %s not valid for: %s", frequency.getCode(), criteriaDetail.getDescription()));
+        }
+    }
+
+    protected void checkAssessmentDetail(CaseType caseType, String section, AssessmentCriteriaEntity assessmentCriteria, ApiAssessmentDetail detail) {
+        AssessmentCriteriaDetailEntity criteriaDetail =
+                assessmentCriteria.getAssessmentCriteriaDetails().stream().filter(
+                        d -> d.getSection().equals(section) && d.getId().equals(detail.getCriteriaDetailId())
+                ).findFirst().orElseThrow(
+                        () -> new ValidationException(
+                                String.format("Section: %s criteria detail item: %d does not exist for criteria id: %s",
+                                        section, detail.getCriteriaDetailId(), assessmentCriteria.getId()))
+                );
+
+        Optional.ofNullable(detail.getApplicantAmount()).orElseThrow(
+                () -> new ValidationException(String.format(
+                        "Criteria detail item with id: %d is missing amount", detail.getCriteriaDetailId())
+                )
+        );
+
+        Frequency applicantFrequency = detail.getApplicantFrequency();
+        if (applicantFrequency != null) {
+            checkCriteriaDetailFrequency(criteriaDetail, applicantFrequency);
+        }
+
+        Frequency partnerFrequency = detail.getPartnerFrequency();
+        if (partnerFrequency != null) {
+            checkCriteriaDetailFrequency(criteriaDetail, partnerFrequency);
+        }
+
+        CaseTypeAssessmentCriteriaDetailValueEntity criteriaDetailValue =
+                caseTypeAssessmentCriteriaDetailValueRepository.findByAssessmentCriteriaDetailAndCaseType(
+                        criteriaDetail, caseType
+                ).orElse(null);
+
+        if (criteriaDetailValue != null) {
+            if (!criteriaDetailValue.getApplicantValue().equals(detail.getApplicantAmount()) ||
+                    !criteriaDetailValue.getApplicantFrequency().getCode().equals(detail.getApplicantFrequency().getCode()) ||
+                    !criteriaDetailValue.getPartnerValue().equals(detail.getPartnerAmount()) ||
+                    !criteriaDetailValue.getPartnerFrequency().getCode().equals(detail.getPartnerFrequency().getCode())) {
+                throw new ValidationException("Incorrect amount entered for: " + criteriaDetail.getDescription());
+            }
         }
     }
 }
