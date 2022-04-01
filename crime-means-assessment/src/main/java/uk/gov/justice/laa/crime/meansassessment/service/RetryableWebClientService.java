fbe04a6a87101d3a3b5e355dee6797952ee64c3a
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-04-01 11:03:05 +0100

    Created a RetryableWebClientService class to hold retry logic for web client calls.

---
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/RetryableWebClientService.java
@@ -0,0 +1,51 @@
+package uk.gov.justice.laa.crime.meansassessment.service;
+
+import io.sentry.Sentry;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import uk.gov.justice.laa.crime.meansassessment.config.RetryConfiguration;
+import uk.gov.justice.laa.crime.meansassessment.exception.APIClientException;
+
+import java.time.Duration;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class RetryableWebClientService {
+
+    @Autowired
+    private RetryConfiguration retryConfiguration;
+
+    public <T> T callWithRetry(Class<T> responseClass, WebClient.ResponseSpec baseResponseSpec, String baseErrorMessage) {
+        return baseResponseSpec
+                .onStatus(
+                        HttpStatus::is5xxServerError,
+                            error -> Mono.error(
+                                    new HttpServerErrorException(
+                                            HttpStatus.valueOf(error.rawStatusCode()),
+                                            baseErrorMessage + " Server Error.")
+                            )
+                )
+                .bodyToMono(responseClass)
+                .retryWhen(
+                        Retry.backoff(
+                                retryConfiguration.getMaxRetries(),
+                                        Duration.ofSeconds(
+                                                retryConfiguration.getMinBackOffPeriod()
+                                        )
+                                )
+                                .jitter(retryConfiguration.getJitterValue())
+                                .filter(
+                                        throwable -> throwable instanceof HttpServerErrorException
+                                )
+                )
+                .onErrorMap(throwable -> new APIClientException(baseErrorMessage, throwable))
+                .doOnError(Sentry::captureException)
+                .block();
+    }
+}
