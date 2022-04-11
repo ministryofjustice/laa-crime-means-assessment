9edae77c94aa7fa825f04a08cb0e5c56359b4d11
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2022-04-11 14:59:32 +0100

    LASB-1013| updated loggers, renamed weightingId

--- crime-means-assessment/src/test/java/uk/gov/justice/laa/crime/meansassessment/service/AssessmentCriteriaChildWeightingServiceTest.java
+++ crime-means-assessment/src/test/java/uk/gov/justice/laa/crime/meansassessment/service/AssessmentCriteriaChildWeightingServiceTest.java
@@ -8,13 +8,11 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.justice.laa.crime.meansassessment.data.builder.TestModelDataBuilder;
 import uk.gov.justice.laa.crime.meansassessment.exception.ValidationException;
 import uk.gov.justice.laa.crime.meansassessment.model.common.ApiAssessmentChildWeighting;
-import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaChildWeightingEntity;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaEntity;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -50,7 +48,7 @@ public class AssessmentCriteriaChildWeightingServiceTest {
     @Test
     public void givenMissingChildWeighting_whenGetTotalChildWeightingIsInvoked_thenThrowsException() {
         List<ApiAssessmentChildWeighting> childWeightings = TestModelDataBuilder.getAssessmentChildWeightings();
-        childWeightings.get(0).setWeightingId(0);
+        childWeightings.get(0).setChildWeightingId(0);
         assertThatThrownBy(
                 () -> criteriaChildWeightingService.getTotalChildWeighting(childWeightings, assessmentCriteria)
         ).isInstanceOf(ValidationException.class).hasMessageContaining("Invalid child weighting id:");
