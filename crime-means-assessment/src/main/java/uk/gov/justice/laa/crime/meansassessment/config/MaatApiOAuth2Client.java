c6e02aa142028fc46a67b835ef476c53a9271e58
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-04-28 17:00:40 +0100

    Remove redundant bean creation in OAuth2 configuration

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/config/MaatApiOAuth2Client.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/config/MaatApiOAuth2Client.java
@@ -1,18 +1,14 @@
 package uk.gov.justice.laa.crime.meansassessment.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.*;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
@@ -40,79 +36,28 @@ public class MaatApiOAuth2Client {
         this.retryConfiguration = retryConfiguration;
     }
 
-    /**
-     * @param tokenUri
-     * @param clientId
-     * @param clientSecret
-     * @return
-     */
-    @Bean
-    ClientRegistrationRepository getRegistration(
-            @Value("${spring.security.oauth2.client.provider.maatapi.token-uri}") String tokenUri,
-            @Value("${spring.security.oauth2.client.registration.maatapi.client-id}") String clientId,
-            @Value("${spring.security.oauth2.client.registration.maatapi.client-secret}") String clientSecret
-    ) {
-        ClientRegistration registration = ClientRegistration
-                .withRegistrationId(REGISTERED_ID)
-                .tokenUri(tokenUri)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .build();
-        return new InMemoryClientRegistrationRepository(registration);
-    }
-
-
-    /**
-     * @param clientRegistrationRepository
-     * @return
-     */
-    @Bean
-    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository) {
-
-        // grant_type = client_credentials flow.
-        OAuth2AuthorizedClientProvider authorizedClientProvider =
-                OAuth2AuthorizedClientProviderBuilder.builder()
-                        .clientCredentials()
-                        .build();
-
-        // Machine to machine service.
-        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager =
-                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
-                        clientRegistrationRepository, new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository));
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-
-        return authorizedClientManager;
-    }
-
-
-    /**
-     * @param authorizedClientManager
-     * @return
-     */
     @Bean(name = "maatAPIOAuth2WebClient")
-    public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultClientRegistrationId(REGISTERED_ID);
-
+    public WebClient webClient(ClientRegistrationRepository clientRegistrations, OAuth2AuthorizedClientRepository authorizedClients) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrations, authorizedClients
+                );
+        oauth.setDefaultClientRegistrationId(REGISTERED_ID);
         WebClient.Builder client = WebClient.builder()
                 .filter(loggingRequest())
                 .filter(loggingResponse())
                 .baseUrl(config.getBaseUrl())
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .filter(retryFilter())
                 .filter(errorResponse());
+
         if (config.isOAuthEnabled()) {
-            client.filter(oauth2Client);
+            client.filter(oauth);
         }
         return client.build();
     }
 
-    /**
-     * @return
-     */
     private ExchangeFilterFunction loggingRequest() {
         return (clientRequest, next) -> {
             log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
@@ -122,9 +67,6 @@ public class MaatApiOAuth2Client {
         };
     }
 
-    /**
-     * @return
-     */
     private ExchangeFilterFunction loggingResponse() {
         return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
             log.info("Response status: {}", clientResponse.statusCode());
