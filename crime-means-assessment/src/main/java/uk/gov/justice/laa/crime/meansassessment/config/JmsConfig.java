4d9dfb51539136004015fb2766d4fd9c2e07e046
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk>
2022-06-26 11:09:06 +0530

    Remove unused beans

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/config/JmsConfig.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/config/JmsConfig.java
@@ -2,18 +2,12 @@ package uk.gov.justice.laa.crime.meansassessment.config;
 
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.destination.DynamicDestinationResolver;
-import uk.gov.justice.laa.crime.meansassessment.handler.JmsErrorHandler;
-
-import javax.jms.Session;
 
 
 /**
@@ -25,31 +19,8 @@ import javax.jms.Session;
 @EnableJms
 public class JmsConfig {
 
-    private final JmsErrorHandler jmsErrorHandler;
 
     private final AmazonSQSConfig amazonSQSConfig;
-
-
-    /**
-     * Use the default container configured.
-     *
-     * @return
-     *
-     */
-
-    @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-
-        DefaultJmsListenerContainerFactory factory =
-                new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(sqsConnectionFactory());
-        factory.setDestinationResolver(new DynamicDestinationResolver());
-        factory.setConcurrency("1");
-        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
-        factory.setErrorHandler(jmsErrorHandler);
-        return factory;
-
-    }
 
     /**
      * Create the jms template with provider config for the SQS client.
