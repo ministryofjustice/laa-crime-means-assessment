67b05f9d7c3bd43d78c529a29a866061482c9738
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-08-05 17:58:17 +0100

    Tests for MeansAssessmentServiceFactory class

---
+++ crime-means-assessment/src/test/java/uk/gov/justice/laa/crime/meansassessment/factory/MeansAssessmentServiceFactoryTest.java
@@ -0,0 +1,40 @@
+package uk.gov.justice.laa.crime.meansassessment.factory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.justice.laa.crime.meansassessment.service.AssessmentCriteriaChildWeightingService;
+import uk.gov.justice.laa.crime.meansassessment.service.FullMeansAssessmentService;
+import uk.gov.justice.laa.crime.meansassessment.service.InitMeansAssessmentService;
+import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.AssessmentType;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest()
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {
+        InitMeansAssessmentService.class,
+        FullMeansAssessmentService.class,
+        MeansAssessmentServiceFactory.class,
+        AssessmentCriteriaChildWeightingService.class,
+})
+public class MeansAssessmentServiceFactoryTest {
+
+    @Autowired
+    private MeansAssessmentServiceFactory meansAssessmentServiceFactory;
+
+    @Test
+    public void givenInitAssessmentType_whenGetServiceIsInvoked_thenInitAssessmentServiceIsReturned() {
+        assertThat(meansAssessmentServiceFactory.getService(AssessmentType.INIT))
+                .isInstanceOf(InitMeansAssessmentService.class);
+    }
+
+    @Test
+    public void givenFullAssessmentType_whenGetServiceIsInvoked_thenFullAssessmentServiceIsReturned() {
+        assertThat(meansAssessmentServiceFactory.getService(AssessmentType.FULL))
+                .isInstanceOf(FullMeansAssessmentService.class);
+    }
+}
