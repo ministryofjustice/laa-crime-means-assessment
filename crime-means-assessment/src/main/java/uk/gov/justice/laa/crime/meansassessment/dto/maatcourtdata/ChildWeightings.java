cec3ccaf9c7c4e926055a03be8c706d7e5628036
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-08-16 12:46:53 +0100

    Removed redundant methods, allow calling CD API to update completion date

---
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/dto/maatcourtdata/ChildWeightings.java
@@ -0,0 +1,15 @@
+package uk.gov.justice.laa.crime.meansassessment.dto.maatcourtdata;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChildWeightings {
+    private Integer childWeightingId;
+    private Integer noOfChildren;
+}
