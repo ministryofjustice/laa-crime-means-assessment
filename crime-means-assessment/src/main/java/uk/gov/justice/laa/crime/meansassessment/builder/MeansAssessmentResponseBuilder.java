3495b7333dc72b5d4993f490280d3476fa80489d
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-08-17 10:11:58 +0100

    Added missing annotations causing startup failure

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/builder/MeansAssessmentResponseBuilder.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/builder/MeansAssessmentResponseBuilder.java
@@ -1,10 +1,16 @@
 package uk.gov.justice.laa.crime.meansassessment.builder;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.crime.meansassessment.dto.MeansAssessmentDTO;
 import uk.gov.justice.laa.crime.meansassessment.model.common.ApiMeansAssessmentResponse;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.entity.AssessmentCriteriaEntity;
 import uk.gov.justice.laa.crime.meansassessment.staticdata.enums.AssessmentType;
 
+@Slf4j
+@Component
+@AllArgsConstructor
 public class MeansAssessmentResponseBuilder {
 
     public ApiMeansAssessmentResponse build(final Integer financialAssessmentID,
