c0d0735bec0fa3f24bffebb40ffc0278eb9fbaa1
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2022-08-18 11:53:06 +0100

    Re-added deleted methods

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MeansAssessmentService.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MeansAssessmentService.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.crime.meansassessment.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
