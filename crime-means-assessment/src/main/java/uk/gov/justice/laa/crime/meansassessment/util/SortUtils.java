901b7096392b84291e5b63f31be512e34b124354
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2022-10-05 00:31:06 +0100

    LCAM-244|rounding full assessment amount fields

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/util/SortUtils.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/util/SortUtils.java
@@ -11,7 +11,9 @@ public final class SortUtils {
     }
 
     public static <T, U extends Comparable> void sortListWithComparing(List<T> t, Function<T, U> compFunction, Function<T, U> thenCompFunc, Comparator<U> comparator) {
-        t.sort(Comparator.comparing(compFunction, comparator).thenComparing(thenCompFunc, comparator));
+        if (t != null) {
+            t.sort(Comparator.comparing(compFunction, comparator).thenComparing(thenCompFunc, comparator));
+        }
     }
 
     public static <U extends Comparable> Comparator<U> getComparator() {
