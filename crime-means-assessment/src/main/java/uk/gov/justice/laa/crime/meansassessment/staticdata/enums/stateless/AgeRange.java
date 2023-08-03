2077fec6656a96c72fc77b97c8ca3407df4a69db
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-08-03 11:45:44 +0100

    Fix typo in enum

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/stateless/AgeRange.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/staticdata/enums/stateless/AgeRange.java
@@ -11,7 +11,7 @@ public enum AgeRange {
     TWO_TO_FOUR("2-4", 2, 4),
     FIVE_TO_SEVEN("5-7", 5, 7),
     EIGHT_TO_TEN("8-10", 8, 10),
-    ELEVEN_TO_TWELVE("11-13", 11, 12),
+    ELEVEN_TO_TWELVE("11-12", 11, 12),
     THIRTEEN_TO_FIFTEEN("13-15", 13, 15),
     SIXTEEN_TO_EIGHTEEN("16-18", 16, 18);
 
