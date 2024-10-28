ec8e623e4c489d9873855891d8bf364d8d5d6fd3
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2024-10-28 09:41:59 +0000

    log changes

--- crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MaatCourtDataService.java
+++ crime-means-assessment/src/main/java/uk/gov/justice/laa/crime/meansassessment/service/MaatCourtDataService.java
@@ -20,15 +20,14 @@ import java.util.Map;
 public class MaatCourtDataService {
 
     private static final String RESPONSE_STRING = "Response from Court Data API: %s";
-    private static final String REQUEST_STRING = "Request to MAAT Court Data Service: {}";
     private final MaatApiConfiguration configuration;
     @Qualifier("maatApiClient")
     private final RestAPIClient maatAPIClient;
 
     public MaatApiAssessmentResponse persistMeansAssessment(MaatApiAssessmentRequest assessment,
                                                             RequestType requestType) {
-        log.debug(REQUEST_STRING, assessment);
-        
+        log.debug("Request to persist means assessment detail : {}", assessment);
+
         MaatApiAssessmentResponse response;
         String endpoint = configuration.getFinancialAssessmentEndpoints().getByRequestType(requestType);
         if (RequestType.CREATE.equals(requestType)) {
@@ -49,7 +48,7 @@ public class MaatCourtDataService {
     }
 
     public RepOrderDTO updateCompletionDate(DateCompletionRequestDTO dateCompletionRequestDTO) {
-        log.debug(REQUEST_STRING, dateCompletionRequestDTO);
+        log.debug("Request to update completion date detail : {}", dateCompletionRequestDTO);
         RepOrderDTO response = maatAPIClient.post(dateCompletionRequestDTO, new ParameterizedTypeReference<>() {
                 },
                 configuration.getRepOrderEndpoints().getDateCompletionUrl(),
