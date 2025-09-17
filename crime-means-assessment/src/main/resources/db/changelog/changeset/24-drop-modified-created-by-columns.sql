e479b796d2e8c96824493983c7e83b426e881a30
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-09-17 12:29:57 +0100

    Simplifying SQL.

--- crime-means-assessment/src/main/resources/db/changelog/changeset/24-drop-modified-created-by-columns.sql
+++ crime-means-assessment/src/main/resources/db/changelog/changeset/24-drop-modified-created-by-columns.sql
@@ -3,18 +3,22 @@
 
 ALTER TABLE crime_means_assessment.assessment_criteria
     DROP COLUMN IF EXISTS created_by,
+ALTER TABLE crime_means_assessment.assessment_criteria
     DROP COLUMN IF EXISTS modified_by;
 
 ALTER TABLE crime_means_assessment.ass_criteria_detail_freq
-    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS created_by;
+ALTER TABLE crime_means_assessment.ass_criteria_detail_freq
     DROP COLUMN IF EXISTS modified_by;
 
 ALTER TABLE crime_means_assessment.assessment_details
-    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS created_by;
+ALTER TABLE crime_means_assessment.assessment_details
     DROP COLUMN IF EXISTS modified_by;
 
 ALTER TABLE crime_means_assessment.case_type_ass_detail_values
-    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS created_by;
+ALTER TABLE crime_means_assessment.case_type_ass_detail_values
     DROP COLUMN IF EXISTS modified_by;
 
 
