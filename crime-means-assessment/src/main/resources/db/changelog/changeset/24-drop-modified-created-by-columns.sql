277ec66eb6556178099dff7db853bc4bb5f04466
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-09-17 12:44:56 +0100

    Modifying SQL

--- crime-means-assessment/src/main/resources/db/changelog/changeset/24-drop-modified-created-by-columns.sql
+++ crime-means-assessment/src/main/resources/db/changelog/changeset/24-drop-modified-created-by-columns.sql
@@ -1,19 +1,19 @@
 --liquibase formatted sql
 --changeset dedwards:24-drop-modified-created-by-columns.sql
 
-ALTER TABLE crime_means_assessment.assessment_criteria
+ALTER TABLE crime_means_assessment.assessment_details
     DROP COLUMN IF EXISTS created_by;
-ALTER TABLE crime_means_assessment.assessment_criteria
+ALTER TABLE crime_means_assessment.assessment_details
+    DROP COLUMN IF EXISTS modified_by;
+
+ALTER TABLE crime_means_assessment.ass_criteria_details
+    DROP COLUMN IF EXISTS created_by;
+ALTER TABLE crime_means_assessment.ass_criteria_details
     DROP COLUMN IF EXISTS modified_by;
 
 ALTER TABLE crime_means_assessment.ass_criteria_detail_freq
     DROP COLUMN IF EXISTS created_by;
 ALTER TABLE crime_means_assessment.ass_criteria_detail_freq
-    DROP COLUMN IF EXISTS modified_by;
-
-ALTER TABLE crime_means_assessment.assessment_details
-    DROP COLUMN IF EXISTS created_by;
-ALTER TABLE crime_means_assessment.assessment_details
     DROP COLUMN IF EXISTS modified_by;
 
 ALTER TABLE crime_means_assessment.case_type_ass_detail_values
