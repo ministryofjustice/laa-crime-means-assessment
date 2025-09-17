67d949eb51e587cd8b32d05d57074ab8519a30f6
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-09-17 12:20:41 +0100

    Adding sql to drop created/modified columns.

---
+++ crime-means-assessment/src/main/resources/db/changelog/changeset/24-drop-modified-created-by-columns.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+--changeset dedwards:24-drop-modified-created-by-columns.sql
+
+ALTER TABLE crime_means_assessment.assessment_criteria
+    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS modified_by;
+
+ALTER TABLE crime_means_assessment.ass_criteria_detail_freq
+    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS modified_by;
+
+ALTER TABLE crime_means_assessment.assessment_details
+    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS modified_by;
+
+ALTER TABLE crime_means_assessment.case_type_ass_detail_values
+    DROP COLUMN IF EXISTS created_by,
+    DROP COLUMN IF EXISTS modified_by;
+
+
